b7f5b7ce7378a4fe9720bc82b9bced2866288053
Peter Bakondy <pbakondy@gmail.com>
2015-07-21 11:04:24 +0200

    extend test cases

--- test.js
+++ test.js
@@ -14,6 +14,7 @@ describe('coercion.js', function() {
 
   // test cases
   var t = [];
+
   // labels
   var l = [];
 
@@ -31,6 +32,7 @@ describe('coercion.js', function() {
   t.push('-0');       l.push('"-0"');
   t.push('-1');       l.push('"-1"');
   t.push('');         l.push('""');
+  t.push('\n');       l.push('"\\n"');
   t.push(null);       l.push('null');
   t.push(undefined);  l.push('undefined');
   t.push(Infinity);   l.push('Infinity');
@@ -44,14 +46,63 @@ describe('coercion.js', function() {
   t.push('potato');   l.push('"potato"');
   t.push(crazyNum);   l.push('crazyNum');
   t.push(crazyObj);   l.push('crazyObj');
+  t.push(new Boolean());  l.push('<Boolean> constructor');
+  t.push(new Number());   l.push('<Number> constructor');
+  t.push(new String());   l.push('<String> constructor');
+  t.push(new Function()); l.push('<Function> constructor');
+  t.push(function(){});   l.push('<Function> literal');
+  t.push(new Date());     l.push('<Date> constructor');
+  t.push(new RegExp());   l.push('<RegExp> constructor');
+
+  // duplicated list to avoid compare objects to itself
+  var t2 = [];
+  t2.push(true);
+  t2.push(false);
+  t2.push(2);
+  t2.push(1);
+  t2.push(0);
+  t2.push(-0);
+  t2.push(-1);
+  t2.push('true');
+  t2.push('false');
+  t2.push('1');
+  t2.push('0');
+  t2.push('-0');
+  t2.push('-1');
+  t2.push('');
+  t2.push('\n');
+  t2.push(null);
+  t2.push(undefined);
+  t2.push(Infinity);
+  t2.push(-Infinity);
+  t2.push([]);
+  t2.push({});
+  t2.push([[]]);
+  t2.push([0]);
+  t2.push([1]);
+  t2.push(NaN);
+  t2.push('potato');
+  t2.push(crazyNum);
+  t2.push(crazyObj);
+  t2.push(new Boolean());
+  t2.push(new Number());
+  t2.push(new String());
+  t2.push(new Function());
+  t2.push(function(){});
+  t2.push(new Date());
+  t2.push(new RegExp());
+
 
   var len = t.length;
 
   describe('should return the same value as native "==" ', function() {
     for (var i = 0; i < len; i++) {
       for (var j = 0; j < len; j++) {
-        it('( ' + l[i] + ' == ' + l[j] + ' ) => ' + (t[i] == t[j]), function() {
-          expect(eqeq(t[i], t[j])).to.equal(t[i] == t[j]);
+        it('( ' + l[i] + ' == ' + l[j] + ' ) => ' + (t[i] == t2[j]), function() {
+          expect(eqeq(t[i], t2[j])).to.equal(t[i] == t2[j]);
+        });
+        it('( ' + l[i] + ' == !' + l[j] + ' ) => ' + (t[i] == !t2[j]), function() {
+          expect(eqeq(t[i], !t2[j])).to.equal(t[i] == !t2[j]);
         });
       }
     }
